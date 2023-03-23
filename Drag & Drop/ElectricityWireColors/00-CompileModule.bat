bd6ce7b7cfc47c1c9dda3e1bec605151ebd061f2
William Plunkett Jr <joey.plunkett92@yahoo.com>
2023-03-23 15:16:39 -0500

    Updating Mods
    - Updated 3 Scripts that where using Dev Versions and not Release Versions

--- Drag & Drop/ElectricityWireColors/00-CompileModule.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-call MC7D2D ElectricityWireColors.dll /reference:"%PATH_7D2D_MANAGED%\Assembly-CSharp.dll" Harmony\*.cs && ^
-echo Successfully compiled ElectricityWireColors.dll
-
-pause
